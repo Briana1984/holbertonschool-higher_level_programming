01515a67efdf70f6f5c8885cc8d5b435f5fd6122
Briana1984 <brian.a.03@hotmail.com>
2023-05-31 13:31:51 -0500

    add new file 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-number_ASCII = range(97, 123)
-for i in number_ASCII:
+number_ascii = range(97, 123)
+for i in number_ascii:
     if i != 101 and i != 113:
-        print("{}".format(chr(i)), end="")
+        number_ascii = chr(i)
+        print(number_ascii, end="")
      
