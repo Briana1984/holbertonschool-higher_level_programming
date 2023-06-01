a97d41ffacd679beebd5c88e59a5c4ba11444723
Briana1984 <brian.a.03@hotmail.com>
2023-06-01 02:01:17 -0500

    add point 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 99:
-        print(f"{int(i/10)}{i%10}", end=", ")
-    else:
-        print(f"{int(i/10)}{i%10}")
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
         
