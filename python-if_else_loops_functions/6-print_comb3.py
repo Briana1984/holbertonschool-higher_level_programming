ace631bd43334d3d6568ff9107a60b6c26bf8918
Briana1984 <brian.a.03@hotmail.com>
2023-06-01 02:28:52 -0500

    add new file point 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(10):
     for n in range((i + 1), 10):
         if i == 8 and n == 9:
             print("{}{}".format(i, n), end='')
         else:
             print("{}{}, ".format(i, n), end='')
-        print("")
+ print("")
