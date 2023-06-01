3625118bf9921d71b6b27110bd071493baef2f59
Briana1984 <brian.a.03@hotmail.com>
2023-06-01 02:54:46 -0500

    add file point 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        c = ord(i)
+        if c <= 122 and c >= 97:
+            c = chr(c - 32)
+        else:
+            c = chr(c)
+        print(f"{c}", end="")
+    print()
+    
