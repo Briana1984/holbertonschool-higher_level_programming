c39e8c8e44ec25a9d3714f16ba75320da5aba35d
Briana1984 <brian.a.03@hotmail.com>
2023-06-09 15:54:31 -0500

    update

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
     try:
-        raise TypeError("Excepción raised")
+        raise TypeError("Exception raised")
     except TypeError as e:
         print(e)
