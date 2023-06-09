a546304b1d92d3ea45d56d0498ac7ec4fa5905fe
Briana1984 <brian.a.03@hotmail.com>
2023-06-09 15:28:49 -0500

    add file

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    resultList = []
-    for item in range(list_length):
-        try:
-            operacion = (my_list_1[item] / my_list_2[item])
-
-        except TypeError:
-            operacion = 0
-            print("wrong type")
-
-        except ZeroDivisionError:
-            operacion = 0
-            print("division by 0")
-
-        except IndexError:
-            operacion = 0
-            print("out of range")
-
-        finally:
-            resultList.append(operacion)
-
-    return resultList
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
