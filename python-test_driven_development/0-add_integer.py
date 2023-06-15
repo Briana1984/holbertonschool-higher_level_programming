9ae5142948f2d788beb7423a47cb470323a5d372
Briana1984 <brian.a.03@hotmail.com>
2023-06-15 10:55:10 -0500

    add files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -34,12 +34,14 @@ def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError( "b must be an integer")
-    
+        raise TypeError("b must be an integer")
+
     a = int(a)
     b = int(b)
 
     return a + b
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
