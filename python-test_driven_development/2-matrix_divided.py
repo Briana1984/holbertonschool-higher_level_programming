65b505d2bbb0d3b3556661737f8a9e17ef3bd4fb
Briana1984 <brian.a.03@hotmail.com>
2023-06-15 13:28:24 -0500

    add files

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,22 +4,25 @@
 
 def matrix_divided(matrix, div):
     """Function that divides all elements of a matrix"""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
 
-    row_size = len(matrix[0])
-    if not all(len(row) == row_size for row in matrix):
+    if not all(isinstance(row, list) for row in matrix) or not all(
+        isinstance(num, (int, float)) for row in matrix for num in row
+    ):
+        raise TypeError(
+            ("matrix must be a matrix (list of lists) of integers/floats"))
+
+    """ Check if each row of the matrix has the same size """
+
+    if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
 
+    """Validate div input"""
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
+    """ Validate div value"""
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    result_matrix = []
-    for row in matrix:
-        result_row = [round(element / div, 2) for element in row]
-        result_matrix.append(result_row)
+    """ return [[round(elem / div, 2) for elem in item] for item in matrix]"""
 
-    return result_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
