2e6a07ed6127f8843599a25640b7191f2c971415
Briana1984 <brian.a.03@hotmail.com>
2023-06-15 14:26:25 -0500

    add file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,42 +1,12 @@
 #!/usr/bin/python
+""" My name is <first name> <last name>"""
 
 
 def say_my_name(first_name, last_name=""):
-    """_summary_
-
-    Args:
-        first_name (_type_): _description_
-        last_name (str, optional): _description_. Defaults to "".
-
-    Raises:
-        TypeError: _description_
-        TypeError: _description_
-    """
+    """function that prints My name is <first name> <last name>"""
     if not isinstance(first_name, str):
-        """_summary_
-
-        Args:
-            str (_type_): _description_
-
-        Raises:
-            TypeError: _description_
-        """
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
-        """_summary_
-
-        Args:
-            str (_type_): _description_
-
-        Raises:
-            TypeError: _description_
-        """
         raise TypeError('last_name must be a string')
     if first_name:
-        """_summary_
-        """
-        print("My name is {}".format(first_name))
-    else:
-        """_summary_
-        """
-        print("My name is {}".format(last_name))
+        print("My name is {} {}".format(first_name, last_name))
