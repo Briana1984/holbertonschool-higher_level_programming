d4ce79b10cfeeacf766c836043e9265b412d45cd
Briana1984 <brian.a.03@hotmail.com>
2023-06-27 15:39:52 -0500

    add square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""class Square import class Rectangle"""
+"""import class Rectangle"""
 from models.rectangle import Rectangle
 
 
-"""Create class Square"""
+"""Create class"""
 
 
 class Square(Rectangle):
-    """Create constructor for square"""
+    """Create constructor"""
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
