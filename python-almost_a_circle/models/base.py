3e522730d9dc788fe146b1c81d62d7b196c7adef
Briana1984 <brian.a.03@hotmail.com>
2023-06-28 13:31:24 -0500

    update base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,16 +21,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """function json_string for list dictionary"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+            return []
 
-        json_string = "["
-        for i, dictionary in enumerate(list_dictionaries):
-            json_string += "{"
-            for key, value in dictionary.items():
-                json_string += f'"{key}": {json.dumps(value)}, '
-            json_string = json_string.rstrip(", ")
-            json_string += "}, "
-        json_string = json_string.rstrip(", ")
-        json_string += "]"
-
-        return json_string
+        return json.dumps(list_dictionaries)
