0a0223238e894386e1be12a943edaf7de7191b49
Briana1984 <brian.a.03@hotmail.com>
2023-06-28 11:02:44 -0500

    add point 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,3 +26,20 @@ class Square(Rectangle):
         '''Enter the attributes'''
         self.width = value
         self.height = value
+        
+    def update(self, *args, **kwargs):
+        """number of arguments"""
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'size', 'x', 'y']
+
+        if argc > 5:
+            argc = 5
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
