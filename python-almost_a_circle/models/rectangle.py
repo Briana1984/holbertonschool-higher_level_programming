1449c3d369b191587c49476c3aceabffe2b289ae
Briana1984 <brian.a.03@hotmail.com>
2023-06-28 12:48:22 -0500

    add files

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,13 +97,14 @@ class Rectangle(Base):
                     setattr(self, k, v)
 
     """funtion dictionary"""
+
     def to_dictionary(self):
         """return a dictionary"""
 
         return {
             'x': self.x,
-            'y': self.y,
+            'width': self.width,
             'id': self.id,
             'height': self.height,
-            'width': self.width
+            'y': self.y
         }
